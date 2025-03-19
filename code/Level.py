acd6de4cf0e199d506ba38c7ff436c9201d6f26f
Meirielli <merielly.ssousa@gmail.com>
2025-03-18 21:05:17 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg Implementation: parallax

---
+++ code/Level.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from idlelib.outwin import file_line_pats
+
+import pygame
+
+from code.Entity import Entity
+from code.EntityFactory import EntityFactory
+
+
+class Level:
+    def __init__(self, window, name, game_mode):
+        self.window = window
+        self.name = name
+        self.game_mode = game_mode #Modo de jogo
+        self.entity_list: list[Entity] = []
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+
+    def run(self):
+        while True:
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move()
+            pygame.display.flip()
+
+        pass
